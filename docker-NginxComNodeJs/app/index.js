ab6f9739892a3940b4eef84a8c2ab42eb057cd15
william campanari <william.campanari@gmail.com>
2025-02-17 23:05:12 -0300

    Reiniciando os estudos

--- docker-NginxComNodeJs/app/index.js
+++ docker-NginxComNodeJs/app/index.js
@@ -1,46 +1,31 @@
 const express = require('express');
 const mysql = require('mysql');
-
 const app = express();
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
     host: 'db',
-    port: 3306,
     user: 'root',
     password: 'password',
     database: 'fullcycle'
 });
 
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database');
+});
+
 app.get('/', (req, res) => {
-    connection.query('SELECT * FROM people', (err, results) => {
-        if (err) {
-            res.status(500).send('Erro ao consultar o banco de dados');
-            return;
-        }
-
-        const names = results.map(person => person.name);
-
-        res.send(`
-            <h1>Full Cycle Rocks!</h1>
-            <ul>
-                ${names.map(name => `<li>${name}</li>`).join('\n')}
-            </ul>
-            
-        `);
+    let sql = 'INSERT INTO people(name) VALUES ("Full Cycle Rocks!!")';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        sql = 'SELECT * FROM people';
+        db.query(sql, (err, results) => {
+            if (err) throw err;
+            res.send('<h1>Full Cycle Rocks!</h1>' + JSON.stringify(results));
+        });
     });
 });
 
-app.post('/add-name', express.json(), (req, res) => {
-    const { name } = req.body;
-
-    connection.query('INSERT INTO people (name) VALUES (?)', [name], (err) => {
-        if (err) {
-            res.status(500).send('Erro ao adicionar nome no banco de dados');
-            return;
-        }
-
-        res.send('Nome adicionado com sucesso!');
-    });
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
-
-app.listen(3000);
